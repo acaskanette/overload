a10a01f5b38225be30a4bf0976ef069598a36b65
Zachary C <zyselr@gmail.com>
2015-11-21 18:36:55 -0500

    Improved character rotation. Rotates to look where character is moving

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -5,105 +5,91 @@ using UnityEngine.UI;
 public class CharacterManager : MonoBehaviour {
 
 
-    [SerializeField]
-    private const int MAX_NUMBER_OF_LIVES = 5;      // Maximum capacity for lives
-    [SerializeField]
-    private const int STARTING_LIVES = 3;           // How many lives you start the game with
+  [SerializeField]
+  private const int MAX_NUMBER_OF_LIVES = 5;      // Maximum capacity for lives
+  [SerializeField]
+  private const int STARTING_LIVES = 3;           // How many lives you start the game with
 
-    private int currentLives;                       // How many lives you have right now
+  private int currentLives;                       // How many lives you have right now
 
-    private StateManager stateManager;
+  private StateManager stateManager;
 
-    [SerializeField]
-    private Animator animator;
-   
-    [SerializeField]
-    private AudioClip shutdownSound;
-    [SerializeField]
-    private Text livesText;
+  [SerializeField]
+  private Animator animator;
 
-    [SerializeField]
-    private GameObject UICanvas;
-    
+  [SerializeField]
+  private AudioClip shutdownSound;
+  [SerializeField]
+  private Text livesText;
 
-
-	// Use this for initialization
-	void Start () {
-        currentLives = STARTING_LIVES;              // Initialize number of lives
-        stateManager = GameObject.FindGameObjectWithTag("Manager").GetComponent<StateManager>();
-        animator.SetBool("hasDied", false);      
-            
-	}
+  [SerializeField]
+  private GameObject UICanvas;
 
 
 
-    // Something hit the player, so let's process that
-    void OnTriggerEnter(Collider _other)
+  // Use this for initialization
+  void Start() {
+    currentLives = STARTING_LIVES;              // Initialize number of lives
+    stateManager = GameObject.FindGameObjectWithTag("Manager").GetComponent<StateManager>();
+    animator.SetBool("hasDied", false);
+
+  }
+
+
+
+  // Something hit the player, so let's process that
+  void OnTriggerEnter(Collider _other) {
+    if (_other.tag == "Enemy" && !animator.GetBool("hasDied"))      // Oh no! An enemy hit me!
     {
-        if (_other.tag == "Enemy" && !animator.GetBool("hasDied"))      // Oh no! An enemy hit me!
-        {
-            LoseLife();            
-            // Check if permanently dead
-            if (OutOfLives())
-            {
-                stateManager.SetState(StateManager.GameState.GAMEOVER_STATE);   // If I am, Game Over man!
-                Respawn();
-            } 
-            else {
-                // If not, Respawn in the center, send the enemies back into their spawners                
-                animator.SetBool("hasDied", true);
-                _other.gameObject.GetComponentInChildren<Animator>().SetBool("killedPlayer", true);
-                StartCoroutine(OnDeath());
-                
-            }
-        }
-    }
-
-    IEnumerator OnDeath()
-    {
-        yield return new WaitForSeconds(3.0f);
-       
+      LoseLife();
+      // Check if permanently dead
+      if (OutOfLives()) {
+        stateManager.SetState(StateManager.GameState.GAMEOVER_STATE);   // If I am, Game Over man!
         Respawn();
-        stateManager.SetState(StateManager.GameState.RESET_STATE);
-        
+      } else {
+        // If not, Respawn in the center, send the enemies back into their spawners                
+        animator.SetBool("hasDied", true);
+        _other.gameObject.GetComponentInChildren<Animator>().SetBool("killedPlayer", true);
+        StartCoroutine(OnDeath());
+
+      }
+    }
+  }
+
+  IEnumerator OnDeath() {
+    yield return new WaitForSeconds(3.0f);
+
+    Respawn();
+    stateManager.SetState(StateManager.GameState.RESET_STATE);
+  }
+
+  public void ResetLives() {
+    currentLives = STARTING_LIVES;
+    livesText.text = currentLives.ToString();
+  }
+
+  void LoseLife() {
+    if (currentLives > 0) {
+      currentLives--;
+      livesText.text = currentLives.ToString();
+      AudioSource.PlayClipAtPoint(shutdownSound, transform.position);
     }
 
+  }
 
-    public void ResetLives()
-    {
-        currentLives = STARTING_LIVES;
-        livesText.text = currentLives.ToString();
-    }
+  // Respawn the player in the center of the level
+  void Respawn() {
+    gameObject.transform.position = new Vector3(0.0f, 1.28f, 0.0f);
+    animator.SetBool("hasDied", false);
+  }
 
-    void LoseLife() {
+  // Tells me whether I'm out of lives
+  bool OutOfLives() {
+    return (currentLives <= 0);
+  }
+  
+  // Update is called once per frame
+  void Update() {
 
-        if (currentLives > 0)
-        {            
-            currentLives--;
-            livesText.text = currentLives.ToString();
-            AudioSource.PlayClipAtPoint(shutdownSound, transform.position);
-        }            
-    
-    }
-
-    // Respawn the player in the center of the level
-    void Respawn()
-    {
-        gameObject.transform.position = new Vector3(0.0f, 1.28f, 0.0f);        
-        animator.SetBool("hasDied", false);        
-    }
-
-    // Tells me whether I'm out of lives
-    bool OutOfLives()
-    {
-        return (currentLives <= 0);
-    }
-
-    
-
-
-	// Update is called once per frame
-	void Update () {
-	   
-	}
+  }
 }
--- Assets/Scripts/OverloadCharacterControllor.cs
+++ Assets/Scripts/OverloadCharacterControllor.cs
@@ -4,145 +4,102 @@ using System.Collections;
 
 
 public class OverloadCharacterControllor : MonoBehaviour {
-    
+  // Speed    
+  private const float MAX_SPEED = 5f;        // How fast I can go
+  [SerializeField]
+  private float currentSpeed = 0.0f;         // How fast I am going
+  [SerializeField]
+  private float acceleration = 1.0f;         // How fast I accelerate
 
-    // Speed    
-    private const float MAX_SPEED = 5f;        // How fast I can go
-    [SerializeField]
-    private float currentSpeed = 0.0f;         // How fast I am going
-    [SerializeField]
-    private float acceleration = 1.0f;         // How fast I accelerate
-
-    [SerializeField]
-    private float jumpSpeed = 20.0f;           // How fast I jump
-   // private float jumpVelocity = 0.0f;         // Capture vertical speed
-    [SerializeField]
-    private float gravity = 9.8f;             // How fast I fall
-
-
-    const float LOWEST_FLOOR_HEIGHT = 1.28f;
-
-    const float STOPPING_DISTANCE = 0.15f;     // Distance between player and target tile before they stop
-
-    private Vector3 moveDirection;             // of travel 
-    private Vector3 previousMoveDirection;     
-    private Vector3 moveVelocity;              // What gets added to my rigidbody's velocity each frame   
-
-    Vector3 targetPosition;                    // Where I clicked to move
-
-    [SerializeField]
-    private Animator animator;                 // The Animator for the character
-    
-    bool jumping;                              // Am I jumping right now?
-
-    private StateManager stateManager;
+  [SerializeField]
+  private float rotSpeed = 5.0F;
+  [SerializeField]
+  private float jumpSpeed = 20.0f;           // How fast I jump
+                                             // private float jumpVelocity = 0.0f;         // Capture vertical speed
+  [SerializeField]
+  private float gravity = 9.8f;             // How fast I fall
 
 
-	// Use this for initialization
-	void Start () {
+  const float LOWEST_FLOOR_HEIGHT = 1.28f;
 
-        targetPosition = Vector3.zero;
-        moveDirection = Vector3.zero;        
+  const float STOPPING_DISTANCE = 0.15f;     // Distance between player and target tile before they stop
+
+  private Vector3 moveDirection;             // of travel 
+  private Vector3 previousMoveDirection;
+  private Vector3 moveVelocity;              // What gets added to my rigidbody's velocity each frame   
+
+  Vector3 targetPosition;                    // Where I clicked to move
+
+  [SerializeField]
+  private Animator animator;                 // The Animator for the character
+
+  bool jumping;                              // Am I jumping right now?
+
+  private StateManager stateManager;
+
+
+  // Use this for initialization
+  void Start() {
+
+    targetPosition = Vector3.zero;
+    moveDirection = Vector3.zero;
+    jumping = false;
+    stateManager = GameObject.FindGameObjectWithTag("Manager").GetComponent<StateManager>();
+
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+    //print("Transform forward: " + transform.forward);
+    //float angle = 0.0f;
+
+    if (stateManager.currentState == StateManager.GameState.PLAYING_STATE) {
+
+      CharacterController controller = GetComponent<CharacterController>();
+
+      if (controller.isGrounded && !animator.GetBool("hasDied")) {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        // print("H:" + horizontal + "  V:" + vertical);
+
         jumping = false;
-        stateManager = GameObject.FindGameObjectWithTag("Manager").GetComponent<StateManager>();                 
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        moveDirection = new Vector3(horizontal, 0, vertical);
+        // moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection.Normalize();
+        moveDirection *= MAX_SPEED;
 
-        //print("Transform forward: " + transform.forward);
-        //float angle = 0.0f;
+        // Rotate in the direction the character is moving by slerping towards the correct transform direction (forward,
+        // back, right, left) based on WS (vertical) and AD (horizontal) key input
+        // Vertical rotation
+        if (vertical > 0)
+          transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), rotSpeed * Time.deltaTime);
+        else if (vertical < 0)
+          transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), rotSpeed * Time.deltaTime);
+        // Horizontal rotation
+        if (horizontal > 0)
+          transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), rotSpeed * Time.deltaTime);
+        else if (horizontal < 0)
+          transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), rotSpeed * Time.deltaTime);
 
-        if (stateManager.currentState == StateManager.GameState.PLAYING_STATE)
-        {
-
-            CharacterController controller = GetComponent<CharacterController>();
-
-            if (controller.isGrounded && !animator.GetBool("hasDied"))
-            {
-
-                float horizontal = Input.GetAxis("Horizontal");
-
-                float vertical = Input.GetAxis("Vertical");
-                //  print("H:" + horizontal + "  V:" + vertical);
-
-                jumping = false;
-                moveDirection = new Vector3(horizontal, 0, vertical);
-                // moveDirection = transform.TransformDirection(moveDirection);
-                moveDirection.Normalize();
-                moveDirection *= MAX_SPEED;
-
-                Vector3 direction = Vector3.zero;
-
-                if (horizontal > 0.2f)
-                {
-                    //  print("right");
-                    direction = Vector3.right;
-                    //facingAngle = 0.0f;
-                }
-                else if (horizontal < -0.2f)
-                {
-                    // print("left");
-                    direction = Vector3.left;
-                    //facingAngle = 180;
-                }
-                if (vertical > 0.2f)
-                {
-                    // print("up");
-                    direction = Vector3.forward;
-                    //facingAngle = 90;
-                }
-                else if (vertical < -0.2f)
-                {
-                    // print("down");
-                    direction = Vector3.back;
-                    //facingAngle = -90;
-                }
-                //print("Direction: " + direction);
-                if (direction != Vector3.zero)
-                    gameObject.transform.LookAt(gameObject.transform.position + direction);
-                // Debug.Log("movedir: " + moveDirection);
-
-                // print("angle of direction: " + (angle));
-
-                if (Input.GetButton("Jump"))
-                {
-                    moveDirection.y = jumpSpeed;
-                    jumping = true;
-                }
-
-            }
-
-
-
-            animator.SetBool("isJumping", jumping);
-            if (!jumping)
-                animator.SetFloat("moveSpeed", moveDirection.magnitude);
-            else
-                animator.SetFloat("moveSpeed", 0.0f);
-
-
-
-            // Turn to face, using Quaternion Lerp
-            //print(transform.rotation.y);
-
-            //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.up),1.0f);
-
-            moveDirection.y -= gravity * Time.deltaTime;
-
-            if (animator.GetBool("hasDied"))
-            {
-                moveDirection = Vector3.zero;
-            }
-
-            controller.Move(moveDirection * Time.deltaTime);
-
-
+        if (Input.GetButton("Jump")) {
+          moveDirection.y = jumpSpeed;
+          jumping = true;
         }
+      }
 
+      animator.SetBool("isJumping", jumping);
+      if (!jumping)
+        animator.SetFloat("moveSpeed", moveDirection.magnitude);
+      else
+        animator.SetFloat("moveSpeed", 0.0f);
 
-	}    
+      moveDirection.y -= gravity * Time.deltaTime;
 
+      if (animator.GetBool("hasDied"))
+        moveDirection = Vector3.zero;
 
+      controller.Move(moveDirection * Time.deltaTime);
+    }
+  }
 }
